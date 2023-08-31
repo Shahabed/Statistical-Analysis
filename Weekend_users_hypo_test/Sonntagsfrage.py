eda85808f4f0194943108048c265e23bf7801c06
Shahabedin Chatraee-Azizabadi <83550007+Shahabed@users.noreply.github.com>
2023-08-31 11:14:17 +0200

    Update Sonntagsfrage.py

--- Weekend_users_hypo_test/Sonntagsfrage.py
+++ Weekend_users_hypo_test/Sonntagsfrage.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 import time
 import seaborn as sns
 from statsmodels.stats.weightstats import ztest as ztest
+import statsmodels.stats.weightstats as ws
 #-----------------IMPORTANT NOTE----------------------
 # DEPENDING ON WHICH DATAFRAME IS USED WE HAVE TWO MAIN FEATURES THAT CAN ALTERNATIVALY REPLACE
 # feature1="sum_page_views"or feature1= "sum_total_seconds_spent"
@@ -42,8 +43,9 @@ def avg_page_views(df,feature1):
     # weekend_mean=df_weekend["sum_page_views"].mean()
     week_mean=df_week[feature1].mean()
     weekend_mean=df_weekend[feature1].mean()    
-    
-    return week_mean, weekend_mean
+    week_std=df_week[feature1].std()
+    weekend_std=df_weekend[feature1].std() 
+    return week_mean, weekend_mean, week_std, weekend_std
 #------------------------------------------------------------------------------------
 df_week,df_weekend=week_weekend_spl(df)
 
@@ -59,12 +61,26 @@ def plot_dist(df,feature1):
 #  A significant Z-test for the number of characters translated, the size of samples are big enough for that test
 def two_samp_z_test_charact(df,feature1):
     df_week,df_weekend=week_weekend_spl(df)
-    z_stat, p_val= ztest(getattr(df_week,feature1),getattr(df_weekend,feature1))
+    z_stat, p_val= ztest(getattr(df_week,feature1),getattr(df_weekend,feature1),alternative='smaller')
+    #z_stat, p_val= CompareMeans.ztest_ind(alternative='smaller', usevar='unequal')
+
     return z_stat, p_val
+# A more specific z-test for unequal stds
+def alt_z_test(df,feature1):
+    df_week,df_weekend=week_weekend_spl(df)
+    col1 = ws.DescrStatsW(getattr(df_week,feature1))
+    col2 = ws.DescrStatsW(getattr(df_weekend,feature1))
+
+    cm_obj = ws.CompareMeans(col1, col2)
+
+    zstat2, z_pval2 = cm_obj.ztest_ind(alternative='smaller',usevar='unequal')
+    
+    return zstat2, z_pval2
 #------------------------------------------------------------------------------------
 # mean of the page view for the week and weekend
-week_mean, weekend_mean=avg_page_views(df,"sum_total_seconds_spent")
+week_mean, weekend_mean, week_std, weekend_std=avg_page_views(df,"sum_total_seconds_spent")
 # The result of z-test for the number of characters translated 
 z_stat, p_val=two_samp_z_test_charact(df,"sum_total_seconds_spent")
+zstat2, z_pval2=alt_z_test(df,"sum_total_seconds_spent")
 plo=plot_dist(df,"sum_total_seconds_spent")
 print("P-value for sum_of_..=",p_val)
